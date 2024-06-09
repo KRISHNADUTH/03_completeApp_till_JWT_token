9b51851e1d6b2dd69841ba5c09ff86fc24201a45
Krishnaduth <krishnaduth777@gmail.com>
2024-06-09 20:55:54 +0530

    initial commit

---
+++ src/main/java/com/example/EazyBankApp/EazyBankAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.EazyBankApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBankAppApplication.class, args);
+	}
+
+}
